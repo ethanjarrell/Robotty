d51bdcc3df41031963ca0c2dd729377c6716fe65
[Ethan Jarrell] <[ethan.jarrell@gmail.com]>
2017-06-21 09:26:47 -0400

    this is my second commit

--- app/index.js
+++ app/index.js
@@ -3,18 +3,21 @@ const path = require('path');
 const mustacheExpress = require('mustache-express');
 
 const data = require("./data.js");
-
 const app = express();
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
+app.use(express.static('public'));
 app.set('view engine', 'mustache')
+
+
+
 
 app.get('/', function (req, res){
 
 
 
-  res.render('index', { img: data.users[0].avatar })
+  res.render('index', { users: data.users })
 });
 
 app.listen(3000, function () {
